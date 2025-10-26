2768ef840b7be968c28c7e940d1b3f81ade746b5
PogodaIgor <igor.pogoda2010@wp.pl>
2025-10-26 16:30:55 +0100

    a

---
+++ app.js
@@ -0,0 +1,21 @@
+function barca() {
+  document.getElementById("barcaText").innerHTML = "barca";
+  document.getElementById("barcaText").style.color = "darkblue";
+}
+function fcbarca() {
+  document.getElementById("barcaBtn").innerHTML = "FC Barcelona";
+}
+function real() {
+  document.getElementById("realText").innerHTML = "real";
+  document.getElementById("realText").style.color = "white";
+}
+function fcreal() {
+  document.getElementById("realBtn").innerHTML = "Real Madryt";
+}
+function villarreal() {
+  document.getElementById("villarrealText").innerHTML = "villarreal";
+  document.getElementById("villarrealText").style.color = "red";
+}
+function fcvillarreal() {
+  document.getElementById("villarrealBtn").innerHTML = "Villarreal CF";
+}
