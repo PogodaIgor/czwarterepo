6b42132c15f168e948f0da992a3cef97f1193376
PogodaIgor <igor.pogoda2010@wp.pl>
2025-10-26 18:45:51 +0100

    a

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 function barca() {
   document.getElementById("barcaText").innerHTML = "barca";
   document.getElementById("barcaText").style.color = "darkblue";
+  document.getElementById("barcaText").style.backgroundColor = "tomato";
 }
 function fcbarca() {
   document.getElementById("barcaBtn").innerHTML = "FC Barcelona";
+}
+function fc_barca() {
+  document.getElementById("barcaBtn").style.imageRendering = "barca.png";
 }
 function real() {
   document.getElementById("realText").innerHTML = "real";
@@ -12,10 +16,16 @@ function real() {
 function fcreal() {
   document.getElementById("realBtn").innerHTML = "Real Madryt";
 }
+function fc_real() {
+  document.getElementById("realBtn").innerHTML;
+}
 function villarreal() {
   document.getElementById("villarrealText").innerHTML = "villarreal";
   document.getElementById("villarrealText").style.color = "red";
 }
 function fcvillarreal() {
   document.getElementById("villarrealBtn").innerHTML = "Villarreal CF";
+}
+function fc_villarreal() {
+  document.getElementById("villarrealBtn").innerHTML;
 }
